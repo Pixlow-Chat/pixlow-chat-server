482912dfb059c90bd4da2c4629d44e20cff539fe
Amit <abhatna7@stevens.edu>
2021-05-02 14:05:38 -0400

    Joining/leaving rooms (currently only rooms A and B, basic implementation)

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 io.on("connection", socket => {
+    socket.on("join room", room => {
+      console.log(`${socket.id} joined room ${room}.`)
+      socket.join(room)
+    })
+    socket.on("leave room", room => {
+      console.log(`${socket.id} left room ${room.roomID}.`)
+      socket.leave(room)
+    })
     socket.emit("your id", socket.id);
     socket.on("send message", body => {
-        io.emit("message", body)
+        io.to(body.room).emit("message", body)
+        console.log(socket.id, body, body.room)
     })
 })
 
