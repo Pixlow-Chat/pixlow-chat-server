ec5ecb3b27c1f7aae710726705b5a29fe635c2de
Amit <abhatna7@stevens.edu>
2021-05-02 19:55:36 -0400

    backend progress, multiple users, basic attendee list data

--- index.js
+++ index.js
@@ -6,25 +6,42 @@ const socket = require("socket.io");
 const io = socket(server);
 const port = process.env.PORT || 8000;
 
-io.on("connection", socket => {
-    socket.on("join room", room => {
-      console.log(`${socket.id} joined room ${room}.`)
-      socket.join(room)
-    })
-    socket.on("leave room", room => {
-      console.log(`${socket.id} left room ${room.roomID}.`)
-      socket.leave(room)
-    })
-    socket.emit("your id", socket.id);
-    socket.on("send message", body => {
-        io.to(body.room).emit("message", body)
-        console.log(socket.id, body, body.room)
-    })
-})
+let userData = [];
+
+io.on("connection", (socket) => {
+  socket.emit("your id", socket.id);
+  socket.on("join room", (room) => {
+    console.log(`${socket.id} joined room ${room}.`);
+    socket.join(room);
+  });
+  socket.on("leave room", (room) => {
+    // console.log(`${socket.id} left room ${room.roomID}.`);
+    // let i;
+    // for (i = 0; i < userData.length; i++) {
+    //   if (userData[i].id == socket.id) {
+    //     userData.splice(i, 1);
+    //   }
+    // }
+    socket.leave(room);
+    // io.to(info.room).emit("refresh attendees", userData);
+  });
+  socket.on("send message", (body) => {
+    io.to(body.room).emit("message", body);
+    console.log(socket.id, body, body.room);
+  });
+  socket.on("store user", (info) => {
+    // console.log(username, socket.id)
+    userData.push({ name: info.name, id: socket.id, room: info.room });
+    console.log(userData);
+    io.to(info.room).emit("refresh attendees", userData);
+  });
+  // socket.on("remove user", id => {
+
+  // })
+});
 
 app.get("/", (req, res) => {
   res.send("Server has successfully started.");
 });
-
 
 server.listen(port, () => console.log("server is running on port " + port));
