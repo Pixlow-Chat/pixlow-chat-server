6c35c450b9bb646fcc936373b05170aacdfe47ed
Amit <abhatna7@stevens.edu>
2021-05-03 20:48:18 -0400

    Finished backend for project (hopefully)

--- index.js
+++ index.js
@@ -6,23 +6,56 @@ const socket = require("socket.io");
 const io = socket(server);
 const port = process.env.PORT || 8000;
 
-let userData = [];
+let userData_A = [];
+let userData_B = [];
+let userData_C = [];
+let userData_D = [];
 
 io.on("connection", (socket) => {
-  socket.emit("your id", socket.id);
+  // socket.emit("your id", socket.id);
   socket.on("join room", (room) => {
     console.log(`${socket.id} joined room ${room}.`);
     socket.join(room);
+    socket.emit("your id", socket.id);
+    // console.log(socket.id)
   });
   socket.on("leave room", (room) => {
-    // console.log(`${socket.id} left room ${room.roomID}.`);
-    // let i;
-    // for (i = 0; i < userData.length; i++) {
-    //   if (userData[i].id == socket.id) {
-    //     userData.splice(i, 1);
-    //   }
-    // }
-    socket.leave(room);
+    console.log(room)
+    if(room.roomID == 'A'){
+      let i;
+      for (i=0; i < userData_A.length; i++) {
+        if(userData_A[i].id == socket.id){
+          userData_A.splice(i, 1)
+          io.to(room.roomID).emit("refresh attendees", userData_A)
+        }
+      }
+    } else if(room.roomID == 'B'){
+      let i;
+      for (i=0; i < userData_B.length; i++) {
+        if(userData_B[i].id == socket.id){
+          userData_B.splice(i, 1)
+          io.to(room.roomID).emit("refresh attendees", userData_B)
+        }
+      }
+    } else if(room.roomID == 'C'){
+      let i;
+      for (i=0; i < userData_C.length; i++) {
+        if(userData_C[i].id == socket.id){
+          userData_C.splice(i, 1)
+          io.to(room.roomID).emit("refresh attendees", userData_C)
+        }
+      }
+    } else if(room.roomID == 'D'){
+      let i;
+      for (i=0; i < userData_D.length; i++) {
+        if(userData_D[i].id == socket.id){
+          userData_D.splice(i, 1)
+          io.to(room.roomID).emit("refresh attendees", userData_D)
+        }
+      }
+    }
+    // console.log(userData_A)
+    socket.leave(room.roomID);
     // io.to(info.room).emit("refresh attendees", userData);
   });
   socket.on("send message", (body) => {
@@ -31,13 +64,20 @@ io.on("connection", (socket) => {
   });
   socket.on("store user", (info) => {
     // console.log(username, socket.id)
-    userData.push({ name: info.name, id: socket.id, room: info.room });
-    console.log(userData);
-    io.to(info.room).emit("refresh attendees", userData);
+    if(info.room == 'A'){
+      userData_A.push({ name: info.name, id: socket.id, room: info.room, icon: info.icon});
+      io.to(info.room).emit("refresh attendees", userData_A)
+    } else if(info.room == 'B'){
+      userData_B.push({ name: info.name, id: socket.id, room: info.room, icon: info.icon});
+      io.to(info.room).emit("refresh attendees", userData_B)
+    } else if(info.room == 'C'){
+      userData_C.push({ name: info.name, id: socket.id, room: info.room, icon: info.icon});
+      io.to(info.room).emit("refresh attendees", userData_C)
+    } else if(info.room == 'D'){
+      userData_D.push({ name: info.name, id: socket.id, room: info.room, icon: info.icon});
+      io.to(info.room).emit("refresh attendees", userData_D)
+    }
   });
-  // socket.on("remove user", id => {
-
-  // })
 });
 
 app.get("/", (req, res) => {
